6dc3320590ed397391df6870dc5a086de7fa7ef4
Andres Orozco <andres.orozco97@hotmail.com>
2022-05-14 15:10:12 -0400

    orden main.js

--- main.js
+++ main.js
@@ -2,15 +2,10 @@
 mybutton = document.getElementById("btnGoUp");
 hamburgerMenu = document.querySelector('.hamburger');
 
+// Function for active Hamburguer Menu.
 const menuIsActive = () => { hamburgerMenu.classList.toggle('active') };
 
-// Call function
-hamburgerMenu.addEventListener('click', menuIsActive);
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+const scrollFunction = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
@@ -18,9 +13,12 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Call function
+hamburgerMenu.addEventListener('click', menuIsActive);
+window.onscroll = function() {scrollFunction()};
 
